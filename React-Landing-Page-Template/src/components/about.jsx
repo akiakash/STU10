56f52c29294e1bf70e00167f0783c5782c79170d
akash <akiagash12@gmail.com>
2022-08-30 20:29:25 +0530

    css changes

--- React-Landing-Page-Template/src/components/about.jsx
+++ React-Landing-Page-Template/src/components/about.jsx
@@ -1,37 +1,55 @@
 export const About = (props) => {
   return (
-    <div id="about">
+    <div id="about" className="text-center">
       <div className="container">
         <div className="row">
-          <div className="col-xs-12 col-md-6">
+          {/* <div className="col-xs-12 col-md-6">
             {" "}
             <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
-          </div>
-          <div className="col-xs-12 col-md-6">
+          </div> */}
+          <div className="col-xs-12 col-md-12">
             <div className="about-text">
               <h2>About Us</h2>
-              <p>{props.data ? props.data.paragraph : "loading..."}</p>
-              <h3>Why Choose Us?</h3>
-              <div className="list-style">
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {props.data
-                      ? props.data.Why.map((d, i) => (
-                          <li key={`${d}-${i}`}>{d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {props.data
-                      ? props.data.Why2.map((d, i) => (
-                          <li key={`${d}-${i}`}> {d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
-              </div>
+
+              <h3>Terms & Conditions</h3>
+              <h4>Payment</h4>
+              <p>
+                We confirm the project by signing of “Letter of contract” and
+                50% of the payment from your quotation total cost with the
+                travel allowance ; this will secure your booking date. Remaining
+                50% payable before 1 week to the event.  Payments can be made
+                with cash, cheque or via direct deposit. Include quotation
+                number and surname in the transaction description to ensure your
+                payment is recorded. Extension of shooting hours will reflect,
+                extra charges based on your package rate.{" "}
+              </p>
+              <h4>Price Negotiation</h4>
+              <p>
+                Feel free to ask Customized package, which match your budget,
+                the final package price won’t be negotiable, We believe that
+                there is no discount in our hard work..
+              </p>
+              <h4>Copyright & Usage</h4>
+              <p>
+                We retain copyright to all our images and we use few images in
+                our website for promotion of our Website. As an artist we treat
+                each images which captured is a bit of Art which is made by us
+                and thus would need to display it. We never use your images for
+                commercial purpose / selling, its going to be used only to
+                demonstrate our previous works to new clients. If you don’t want
+                to display your images anywhere within the Studio 10 Control, we
+                charge 25% higher photography fee from your total package
+                amount.
+              </p>
+              <h4>Warning</h4>
+              <p>
+                This Website and Its content is Copyrighted to studio10.lk ©.
+                All Rights Reserved. Any Redistribution and Reproduction of part
+                or all the content in any form is prohibited.You may Not, except
+                with our written permission, distribute or commercially exploit
+                the content.Nor May you transmit or store it in any other
+                website or other form of electronic retrieval system.
+              </p>
             </div>
           </div>
         </div>
